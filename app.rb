a26c1f87d4c5f22f78710d344de133352ca07b8d
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-12-15 11:37:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Lizzy"
+  end
+
+  get '/hometown' do
+    "My hometown is Rosly, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'I Turn to You'."
+  end
+
+
+
+
 end
